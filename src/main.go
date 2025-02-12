801f4e645c01dded07de2d1ed443b289b680349b
Izzy Jones <IzzyJonesContact@gmail.com>
2025-02-12 10:45:28 -0500

    Added function to get a row from an SQLite database.

--- src/main.go
+++ src/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Based Go execution frfr, no cap and stuff.")
+
+	var testHash string = `4c3f9505b832a5a8bb22d5d339b1dfd4800d96d3ffec4a495fdc2274efa6601c`
+	var hashResult = checkHash(testHash)
+
+	fmt.Println(hashResult)
 
 	// Pausing so that the docker container doesn't close right away.
 	// This won't be needed when the program has logic to continue to run.
